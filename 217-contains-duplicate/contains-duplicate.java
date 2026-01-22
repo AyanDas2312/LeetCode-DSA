94aaf249c64b3bc309e0c3a1902ff299cd796f82
Ayan Das <ayandas6790@gmail.com>
2026-01-22 22:20:50 +0530

    Time: 38 ms (5.35%) | Memory: 97 MB (16.85%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -4,14 +4,10 @@ class Solution {
         for (int num : nums) {
             dup.put(num, dup.getOrDefault(num, 0) + 1);
         }
-        int count=0;
         for(int key : dup.keySet()){
             if(dup.get(key)>1){
-                count++;
+                return true;
             }
-        }
-        if(count>0){
-            return true;
         }
         return false;
     }
